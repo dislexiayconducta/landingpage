724480e200e6744da7f2cdea6ba6ae55a803300c
jfpanchi <jefpanchi@gmail.com>
2025-08-06 11:35:36 -0500

    feat(landing): 🎉Initialize Next.js project with core files
    Set up a new Next.js project including configuration files, font assets, SVGs, and initial source code structure. Added ESLint, Tailwind, and UI component setup for development.

---
+++ src/app/layout.jsx
@@ -0,0 +1,104 @@
+import "./globals.css";
+import localFont from "next/font/local";
+
+export const neueHaasDisplay = localFont({
+  src: [
+    {
+      path: "../../public/fonts/NeueHaasDisplayXXThin.ttf",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/NeueHaasDisplayXXThinItalic.ttf",
+      weight: "100",
+      style: "italic",
+    },
+    {
+      path: "../../public/fonts/NeueHaasDisplayXThin.ttf",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/NeueHaasDisplayXThinItalic.ttf",
+      weight: "200",
+      style: "italic",
+    },
+    {
+      path: "../../public/fonts/NeueHaasDisplayThin.ttf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/NeueHaasDisplayThinItalic.ttf",
+      weight: "300",
+      style: "italic",
+    },
+    {
+      path: "../../public/fonts/NeueHaasDisplayLight.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/NeueHaasDisplayLightItalic.ttf",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "../../public/fonts/NeueHaasDisplayRoman.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/NeueHaasDisplayRomanItalic.ttf",
+      weight: "500",
+      style: "italic",
+    },
+    {
+      path: "../../public/fonts/NeueHaasDisplayMediu.ttf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/NeueHaasDisplayMediumItalic.ttf",
+      weight: "600",
+      style: "italic",
+    },
+    {
+      path: "../../public/fonts/NeueHaasDisplayBold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/NeueHaasDisplayBoldItalic.ttf",
+      weight: "700",
+      style: "italic",
+    },
+    {
+      path: "../../public/fonts/NeueHaasDisplayBlack.ttf",
+      weight: "900",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/NeueHaasDisplayBlackItalic.ttf",
+      weight: "900",
+      style: "italic",
+    },
+  ],
+  display: "swap",
+  variable: "--font-neue-haas",
+});
+
+export const metadata = {
+  title: "Dislexia y Conducta",
+  description: "Dislexia y Conducta",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="es">
+      <body className={`${neueHaasDisplay.variable} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
